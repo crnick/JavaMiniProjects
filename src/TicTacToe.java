0c3ede9e293f45ed11c1cebfb002eadce17d06ec
nikhil joglekar <nikhil.joglekar@ideaqu.com>
2025-03-16 06:57:18 -0400

    Created a basic board for user input

---
+++ src/TicTacToe.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class TicTacToe {
+    public static void main(String[] args){
+        char [][] board = new char[3][3];
+        for (int row=0;row<board.length;row++){
+            for (int col = 0; col < board[row].length;col++){
+                board[row][col] = ' '; //initializing the board.
+            }
+        }
+
+        char player = 'X';
+        boolean gameOver = false;
+
+        Scanner sc = new Scanner(System.in);
+
+        while(!gameOver){
+            printBoard(board);
+            System.out.println("Player" + player + "enter:");
+            int row = sc.nextInt();
+            int col = sc.nextInt();
+        }
+    }
+
+    public static void printBoard(char [][] board){
+
+    }
+}
